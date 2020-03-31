3493c846b32f6f1e6e10886602ea3fc01b53fb40
Irit <35077819+iritush@users.noreply.github.com>
2020-03-30 20:01:47 -0400

    -changed language query to be an array query on the volunteer side -added some extra checks and respnse status codes

--- functions/index.js
+++ functions/index.js
@@ -10,31 +10,32 @@ exports.updateUserAvailability = functions.https.onRequest((request, response) =
     usersCollection.where('userID', '==', userID).get()
     .then(userSnapshot => {
         if (userSnapshot.empty) {
-            let error = "Did not any documents matching this user";
+            let error = "Did not find any documents matching user id " + userID;
             console.log(error);
-            response.send(error);
+            response.status(404).send(error);
             }
         else {
             db.collection('users').doc(userID).update({CanChat: isAvailable});
         }
     })
-    .catch(err => {
-        console.log("Hit an error when trying to update availablity ", err);
-        response.send(err, 500);
+    .catch(error => {
+        console.log("Hit an unexpected error when trying to update availablity ", err);
+        response.status(500).send(error);
       });
 });
 
+// curl -X POST <local path to firebase fuunction> -H "Content-Type:application/json"  -d '{"userID":"<id>"}'
 exports.matchMeNow = functions.https.onRequest((request, response) => {
     let userID = request.body.userID;
+    // let userID = 'JG39wKP4ffieibDuhx2s';
     let usersCollection = db.collection('users');
     let volunteersCollection = db.collection('volunteers');
-
     usersCollection.doc(userID).get()
     .then(currentUserDoc => {
-        if (currentUserDoc.empty) {
+        if (!currentUserDoc.exists) {
             error = "Did not find any users with id " + userID;
             console.log(error);
-            response.status(500).send(error);
+            response.status(404).send(error);
             }
 
         else {
@@ -43,32 +44,39 @@ exports.matchMeNow = functions.https.onRequest((request, response) => {
                 let currentUserZipCode = currentUserDoc.data().zipCode;
                 let currentUsergenderPreference = currentUserDoc.data().genderPreference;
                 let currentUserLanguage = currentUserDoc.data().language;
+                
                 // might make sense to filter through "CanChat" flag first/create index?
                 if (currentUsergenderPreference != "no preference") {
-                    matchingVolunteersSnapshot = volunteersCollection.where('canChat', '==', true).where('zipCode', '==', currentUserZipCode).where('language', '==', currentUserLanguage).where('gender', '==', currentUsergenderPreference);
+                    matchingVolunteersSnapshot = volunteersCollection.where('languages', 'array-contains', currentUserLanguage).where('canChat', '==', true).where('zipCode', '==', currentUserZipCode).where('gender', '==', currentUsergenderPreference);
                     }
                 else {
-                    matchingVolunteersSnapshot = volunteersCollection.where('canChat', '==', true).where('zipCode', '==', currentUserZipCode).where('language', '==', currentUserLanguage);
+                    matchingVolunteersSnapshot = volunteersCollection.where('languages', 'array-contains', currentUserLanguage).where('canChat', '==', true).where('zipCode', '==', currentUserZipCode);
                     }
                 
                     matchingVolunteersSnapshot.get()
-                .then(matchingVolunteersSnapshot => {
-                    matchingVolunteersSnapshot.forEach(doc=> {
-                        console.log("id: " + doc.id);
-                        matchingVolunteersList.push(doc);
-                        numOfMatches = matchingVolunteersList.length;
-                        console.log("Number of matches: " + numOfMatches);
-                        randomSelction = Math.floor((Math.random() * numOfMatches) + 0);
-                        console.log("Selected entry number: " + randomSelction);
-                        selectedVulunteerUID  = matchingVolunteersList[randomSelction].id;
-                        response.status(200).send(selectedVulunteerUID);
+                    .then(matchingVolunteersSnapshot => {
+                        if (matchingVolunteersSnapshot.empty) {
+                            message = "Could not find any available matches for user " + userID;
+                            console.log(message);
+                            response.status(200).send(null);
+                            }
+                        else {
+                        matchingVolunteersSnapshot.forEach(doc=> {
+                            console.log("id: " + doc.id);
+                            matchingVolunteersList.push(doc);
+                            numOfMatches = matchingVolunteersList.length;
+                            console.log("Number of matches: " + numOfMatches);
+                            randomSelction = Math.floor((Math.random() * numOfMatches) + 0);
+                            console.log("Selected entry number: " + randomSelction);
+                            selectedVulunteerUID  = matchingVolunteersList[randomSelction].id;
+                            response.status(200).send(selectedVulunteerUID);
+                            })
+                        }
                     })
-                });
-                
-            }
+                }
         })
-    .catch(err => {
-        console.log('Hit an error when trying to find a match', error);
+    .catch(error => {
+        console.log('Hit an unexpected error when trying to find a match', error);
         response.status(500).send(error);
   });
 });
