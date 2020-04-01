032c0a87d1ee33bf4126614178c86fe6503029e1
alan.hutcheson <alanhutcheson@deloitte.co.uk>
2020-04-01 09:30:20 +0100

    [AH] Fixed routing - updated names to match documentation (Users and Volunteers) and added UserSignUpPage and VolunteerSignUpPage

--- phone-a-friend-web/src/Paths.tsx
+++ phone-a-friend-web/src/Paths.tsx
@@ -1,6 +1,6 @@
-export const CALLER_PATH = "/caller";
-export const CALLEE_PATH = "/callee";
-export const REGISTER_PATH= "/register";
-export const LOGIN_PATH= "/login";
-export const LOGGED_IN_PATH= "/loggedIn";
-export const CONTACT_PATH= "/contact";
+export const VOLUNTEER_PATH = "/volunteer";
+export const USER_PATH = "/user";
+export const LOGIN_PATH = "/login";
+export const CONTACT_PATH = "/contact";
+export const VOLUNTEER_SIGN_UP_PATH = "/register/volunteer";
+export const USER_SIGN_UP_PATH = "/register/user";
