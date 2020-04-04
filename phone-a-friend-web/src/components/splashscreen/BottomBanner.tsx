9cebf3e59d901c44f5b795cece73fa92165c81f0
mike connor <mike@rocketnumber9.org>
2020-04-04 18:25:22 -0500

    Bottom Banner
    MainDiv had two flex setting.  First row, then column.  Should be row.
    
    changed styled-media-query to lessThan medium, remove the whole component.  But that is just basd on the desings I’ve seen.  Perhaps there are some small-to-medium size screens?
    
    changed the outside margin to be a % rather than pixels.

--- phone-a-friend-web/src/components/splashscreen/BottomBanner.tsx
+++ phone-a-friend-web/src/components/splashscreen/BottomBanner.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import styled from "styled-components";
-import { OUTSIDE_MARGIN } from "../../utils/Constants";
+import * as Constants from "../../utils/Constants";
 import * as Colors from "../../Colors";
 import media from "styled-media-query";
 import Button from "../buttons/Button";
 
 interface Iprops {
-  outsideMargin?: string;
-  bgColor?: string;
+  outsideMargin: string;
+  bgColor: string;
 }
 
 const MainDiv = styled.div<Iprops>`
@@ -19,24 +19,18 @@ const MainDiv = styled.div<Iprops>`
   padding-left: ${(props) => props.outsideMargin};
   height: 100%;
   background: ${(props) => props.bgColor};
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-
-  ${media.greaterThan("medium")`
-    display: grid;
-    grid-template-columns: 1fr 2fr 1fr;
+  ${media.lessThan("medium")`
+    display:none;
   `}
 `;
 
 const Input = styled.input`
-  width: 100%;
-  margin-bottom: 8px;
+  width: 44%;
 `;
 
 const BottomBanner = () => {
   return (
-    <MainDiv outsideMargin={OUTSIDE_MARGIN} bgColor={Colors.LIGHT_BLUE}>
+    <MainDiv outsideMargin={Constants.OUTSIDE_MARGIN} bgColor={Colors.LIGHT_BLUE}>
       <h2>Stay Connected</h2>
       <Input
         id="namedInput"
