ff85b0b1af3eb14115bf9ec2042ccc873c36980b
Irit <35077819+iritush@users.noreply.github.com>
2020-04-05 12:47:08 -0400

    Added environment configs to the scheduled match function and changed the function name to a more generic name: scheduledmatchUsersAndVolunteers (was: scheduledAirtableMatcher)

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,4 @@
 const { functions } = require("./src/config");
-
 const airTableMatchingFunction = require("./src/match/airtable");
 const firestoreMatchingFunction = require("./src/match/firestore");
 const updateUserAvailability = require("./src/update/userAvailability");
@@ -16,8 +15,8 @@ const matchUsersAndVolunteers = require("./src/match/airtableScheduledMatch");
 /**
  * Run at 8am, 10am, 12pm, 2pm, 4pm, 6pm, 8pm, 10pm
  */
-exports.scheduledAirtableMatcher = functions.pubsub.schedule('every 2 hours from 08:00 to 23:59')
-  .timeZone('Europe/London')
+exports.scheduledmatchUsersAndVolunteers = functions.pubsub.schedule(functions.config().scheduledmatchvariables.intervals)
+  .timeZone(functions.config().scheduledmatchvariables.timezone)
   .onRun((context) => {
     matchUsersAndVolunteers();
     return null;
