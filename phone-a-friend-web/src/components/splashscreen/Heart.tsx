605cf0cd6d6552bb14614e2b8cfbaaeffb1a2821
mike connor <mike@rocketnumber9.org>
2020-04-05 16:41:16 -0500

    using SVG art on splash page

---
+++ phone-a-friend-web/src/components/splashscreen/Heart.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import HeartImage from '../../images/heart.svg'
+const Heart = () => {
+    return (
+        <div>
+            <img src={HeartImage} alt="hand" />
+        </div>
+    )
+}
+
+export default Heart
--- phone-a-friend-web/src/components/splashscreen/MainSplash.tsx
+++ phone-a-friend-web/src/components/splashscreen/MainSplash.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import styled from "styled-components";
 import CallOrReceive from "./CallOrReceive";
+import Heart from "./Heart";
 import MainJumbo from "./MainJumbo";
 import BottomBanner from "./BottomBanner";
 import * as Colors from "../../Colors";
+import * as Constants from "../../utils/Constants";
 import { USER_PATH, VOLUNTEER_PATH } from "../../Paths";
 import media from "styled-media-query";
 
@@ -24,16 +26,35 @@ const IwantDiv = styled.div`
   `}
 `;
 
+type Iprops= {
+    top: number;
+}
+const HeartDiv = styled.div<Iprops>`
+    position:absolute;
+    z-index:2;
+    left: 50%;
+    transform: translateX(-50%);
+    top: ${(props) => `${props.top}px`};
+    ${media.lessThan("medium")`
+        display:none;
+    `}
+`;
+
 const MainSplash: React.FunctionComponent = () => (
     <MainDiv>
         <div>
             <IwantDiv>
+                <HeartDiv top={Constants.HEADER_HEIGHT + 150}>
+                    <Heart />
+                </HeartDiv>
                 <CallOrReceive
+                     h={Constants.SPLASH_IWANT}
                     bgColor={Colors.PEACH}
                     btnCopy="Phone a Friend"
                     path={USER_PATH}
                 />
                 <CallOrReceive
+                    h={Constants.SPLASH_IWANT}
                     bgColor={Colors.LAVENDER}
                     btnCopy="Take a Call"
                     path={VOLUNTEER_PATH}
