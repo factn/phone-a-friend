cbb06ffdfe27dd69ebbf414dfefb9e567b3dfc90
mike connor <mike@rocketnumber9.org>
2020-04-05 12:19:20 -0500

    Constants

--- phone-a-friend-web/src/utils/Constants.tsx
+++ phone-a-friend-web/src/utils/Constants.tsx
@@ -1,1 +1,5 @@
-export  const OUTSIDE_MARGIN = '5%';
+export const OUTSIDE_MARGIN = '5%';
+export const HEADER_HEIGHT = 92;
+export const JUMBO_HEIGHT = 150;
+export const SIGNUP_HEIGHT = 150;
+export const FOOTER_HEIGHT = 50;
