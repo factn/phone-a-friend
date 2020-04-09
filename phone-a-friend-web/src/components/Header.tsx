64973bac0f39a12033aefef85bc300dbf572b962
mike connor <mike@rocketnumber9.org>
2020-04-09 11:27:18 -0500

    mobile hearder, Constant

--- phone-a-friend-web/src/components/Header.tsx
+++ phone-a-friend-web/src/components/Header.tsx
@@ -14,8 +14,11 @@ const Container = styled.div<Iprops>`
   width: 100%;
   height: ${(props) => `${props.h}px`};
   white-space: nowrap;
+  border: blue 1px solid;
+  
 `;
 const MainDiv = styled.div<Iprops>`
+    height: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
--- phone-a-friend-web/src/utils/Constants.tsx
+++ phone-a-friend-web/src/utils/Constants.tsx
@@ -5,3 +5,6 @@ export const SIGNUP_HEIGHT = 150;
 export const FOOTER_HEIGHT = 50;
 export const SPLASH_JUMBO = 500;
 export const SPLASH_IWANT = 500;
+
+export const DESKTOP = 'desktop';
+export const MOBILE = 'mobile';
